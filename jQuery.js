82bae3842b190b62b1ef615ba7073c3e949258a0
Gada Wako <gwak1801@gmail.com>
2020-06-20 14:56:02 -0500

    add event listener

---
+++ jQuery.js
@@ -0,0 +1,7 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $("#submit").click(function(){
+        alert("Submit was clicked.");
+      });
+});

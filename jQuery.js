739099c8d6fe79da4cd1b14c3b831e63369af3c6
Gada Wako <gwak1801@gmail.com>
2020-06-20 23:02:45 -0500

    delete button

--- jQuery.js
+++ jQuery.js
@@ -1,7 +1,16 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    console.log( "ready!" );
-    $("#submit").click(function(){
-        alert("Submit was clicked.");
+    console.log( 'ready!' );
+    $('#submit').click(function(event){ 
+        event.preventDefault()
+        let firstName = $('#firstName').val()
+        let lastName = $('#lastName').val()
+        let id = $('#id').val()
+        let jobTitle = $('#jobTitle').val()
+        let aSalary = $('#aSalary').val()
+        let userInput = "<tr><td>" + firstName + "</td><td>" 
+        + lastName + "</td><td>" + id + "</td><td>" + jobTitle + "</td><td>"  
+        + aSalary + "</td><td>" + "<input type= 'submit' id='delete' value='delete'></td></tr>"
+        $("table tbody").append(userInput)
       });
 });

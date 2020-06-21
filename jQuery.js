998c91ff277b9287647dfce8e33d6d31fe2b9a68
Gada Wako <gwak1801@gmail.com>
2020-06-21 16:35:05 -0500

    adding css adn delete button

--- jQuery.js
+++ jQuery.js
@@ -1,6 +1,4 @@
-// A $( document ).ready() block.
 $( document ).ready(function() {
-    console.log( 'ready!' );
     $('#submit').click(function(event){ 
         event.preventDefault()
         let firstName = $('#firstName').val()
@@ -9,8 +7,21 @@ $( document ).ready(function() {
         let jobTitle = $('#jobTitle').val()
         let aSalary = $('#aSalary').val()
         let userInput = "<tr><td>" + firstName + "</td><td>" 
-        + lastName + "</td><td>" + id + "</td><td>" + jobTitle + "</td><td>"  
+        + lastName + "</td><td>" + id + "</td><td>" + jobTitle + "</td><td class='aSalary'>"  
         + aSalary + "</td><td>" + "<input type= 'submit' id='delete' value='delete'></td></tr>"
-        $("table tbody").append(userInput)
-      });
-});
+        $("table tbody").append(userInput);
+
+        let  annualSalary = $('.aSalary') 
+        let totalAnnualSalary
+        console.log(annualSalary)
+        for(let i = 0; i < annualSalary.length; i++) {
+            totalAnnualSalary = totalAnnualSalary + annualSalary[i]
+            console.log(annualSalary[i].innerText)
+        }
+
+        
+        $("table").on('click', '#delete', function () {
+            $(this).closest('tr').remove();
+        })
+    })
+})
